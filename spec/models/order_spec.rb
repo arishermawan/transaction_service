27fd24ea1e388776640d5a141a87acbbdc04b66d
rizhima <arishermawan@hotmail.com>
2017-12-20 11:17:26 +0700

    fix spec

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -27,4 +27,28 @@ RSpec.describe Order, type: :model do
     order.valid?
     expect(order.errors[:service]).to include("is not included in the list")
   end
+
+  context "subtract and add gopay after order" do
+    before :each do
+      @gopay1 = Gopay.new.add_record(20000.0, 1, 0)
+      @gopay2 = Gopay.new.add_record(20000.0, 1, 1)
+      @order = create(:order, payment:'gopay', customer_id:1, driver_id:1, total:10000.0)
+      @order.subtract_gopay
+      @order.add_gopay
+      @gopay1.reload
+      @gopay2.reload
+    end
+
+    describe "subtract gopay" do
+      it "reduce customer gopay" do
+        expect(@gopay1.credit).to eq(10000.0)
+      end
+    end
+
+    describe "subtract gopay" do
+      it "reduce customer gopay" do
+        expect(@gopay2.credit).to eq(30000.0)  
+      end
+    end
+  end
 end
